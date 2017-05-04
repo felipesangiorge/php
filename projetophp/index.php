fce35e72d16949491f65e645032bf1e0b152cf8f
Felipe Sangiorge <felipe.sangiorge@gmail.com>
2017-05-04 17:58:58 -0300

    final

--- projetophp/index.php
+++ projetophp/index.php
@@ -1,12 +1,33 @@
 <?php 
 
-$nome =(int)$_GET["a"];
-$idade = (int)$_GET["b"];
-var_dump($nome,$idade);
+//$nome =(int)$_GET["a"];
+//$idade = (int)$_GET["b"];
+//var_dump($nome,$idade);
 
-$ip = $_SERVER["SERVER_ADDR"];
+//$ip = $_SERVER["SERVER_ADDR"];
 
-var_dump($ip);
+//var_dump($ip);
+
+?>
+
+<form>
+
+<input type="text" name="nome">
+<input type="date" name="nascimento">
+<input type="submit" name="OK">
+
+</form>
+
+<?php 
+if (isset($_GET)){
+	foreach ($_GET as $key => $value){
+		echo "nome do campo $key"."<br>";
+		echo "valor do campo $value";
+		echo "<hr>";
+		
+	}
+}
+
+?>
 
 
- ?>
