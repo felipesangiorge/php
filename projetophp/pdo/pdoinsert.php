ba944e82037bed73459a0ddee8c326f60cebe44c
felipe sangiorge <felipe.sangiorge@gmail.com>
2017-05-07 22:08:13 -0300

    PDO_

--- projetophp/pdo/pdoinsert.php
+++ projetophp/pdo/pdoinsert.php
@@ -1,11 +1,29 @@
+<!DOCTYPE HTML>
+	<html>
+	
+	<head></head>
+	
+		<body>
+		
+				<input type="text" value="<?php echo $usuario ?> placeholder="login">
+				
+				<input type="text" value="<?php echo $senha ?> placeholder="login">
+				
+				<button type="submit" class="button" name="myButton" value="foo">Entrar</button>
+		
+		</body>
+	
+	
+	</html>
+
 <?php
 
 require_once 'config.php';
 
 $stmt = $conn -> prepare ("INSERT INTO tb_usuarios (des_nome,des_senha) VALUES (:DESNOME,:DESSENHA)");
 
-$login = "Camila";
-$password = "1234556";
+$login = $usuario;
+$password = $senha;
 
 $stmt -> bindParam(":DESNOME",$login);
 $stmt -> bindParam(":DESSENHA",$password);
